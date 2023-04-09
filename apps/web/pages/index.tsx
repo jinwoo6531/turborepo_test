9d2bfc37f16f1688e76446e77e971bf1bb065132
jinwoo6531 <jintakk6531@gmail.com>
2023-04-09 16:12:51 +0900

    feat: 라이브러리 & ui component 의존성 테스트

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Button } from 'ui';
+import { Button, Input } from 'ui';
 
 export default function Web() {
   return (
     <div>
       <h1>2번플젝</h1>
       <Button />
+      <Input />
     </div>
   );
 }
